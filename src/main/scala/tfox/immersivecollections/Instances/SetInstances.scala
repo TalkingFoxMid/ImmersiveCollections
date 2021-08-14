d713dbd9003bc1546ddc0b5548fdc94f26bbb12e
TalkingFoxMid <futureinrevolution@gmail.com>
2021-08-15 00:14:25 +0500

    Set: FlatMap

--- src/main/scala/tfox/immersivecollections/Instances/SetInstances.scala
+++ src/main/scala/tfox/immersivecollections/Instances/SetInstances.scala
@@ -1,9 +1,9 @@
 package tfox.immersivecollections.Instances
-import cats.{Applicative, Eval, Functor, Id, Monad, Traverse, instances}
+import cats.{Applicative, Eval, FlatMap, Functor, Id, Monad, Traverse, instances}
 import cats.syntax.functor._
 
 object SetInstances {
-  implicit val traverse: Traverse[Set] = new Traverse[Set] {
+  implicit val traverse: Traverse[Set] with FlatMap[Set] = new Traverse[Set] with FlatMap[Set] {
     override def traverse[G[_], A, B](fa: Set[A])(f: A => G[B])(implicit evidence$1: Applicative[G]): G[Set[B]] =
       fa.foldLeft(Applicative[G].pure(Set.empty[B])) {
         case (a, b)=> Applicative[G].product(a, f(b))
@@ -17,5 +17,11 @@ object SetInstances {
 
     override def foldRight[A, B](fa: Set[A], lb: Eval[B])(f: (A, Eval[B]) => Eval[B]): Eval[B] =
       fa.foldLeft(lb)((a,b) => f(b,a))
+
+    override def flatMap[A, B](fa: Set[A])(f: A => Set[B]): Set[B] =
+      fa.flatMap(f)
+
+    override def tailRecM[A, B](a: A)(f: A => Set[Either[A, B]]): Set[B] =
+      f(a).collect { case Right(value) => value}
   }
 }
